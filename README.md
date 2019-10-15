74873c66f7ea3e0a9b0cb38bcca005ea50a3bc4f
Marcel Bonnet <marcelbonnet@anatel.gov.br>
2019-10-14 22:57:54 -0300

    fixes

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Tutorial de Plugin para Redmine
 
+Persistir o mapeamento ORM no banco de dados:
+`# bundle exec rake redmine:plugins:migrate`
 
+`# grep RAILS_ENV ~/.cshrc   
+setenv  RAILS_ENV production `
+
+`# echo $RAILS_ENV
+production `
+
+`# while true; do rails s -p 3000 -b 0.0.0.0 -e production; done`
 
 # Problemas
 
--- init.rb
+++ init.rb
@@ -1,8 +1,22 @@
 Redmine::Plugin.register :teste do
-  name 'Plugin de Teste'
+  name 'Redmine - Plugin Tutorial'
   author 'Marcel Bonnet'
-  description 'Plugin para testar o Redmine'
+  description 'Tutorial de plugin para Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/marcelbonnet/tutorial-redmine-plugin'
+  author_url 'https://github.com/marcelbonnet/'
+
+  # http://www.redmine.org/projects/redmine/wiki/Plugin_Tutorial
+  # Syntax:
+  # menu(menu_name, item_name, url, options={})
+  
+  # #############
+  # Para aparecer no menu de um projeto
+  # #############
+  # permission :carros, { :carros => [:index, :new, :edit] }, :public => true
+  # menu :project_menu, :carros, { :controller => 'carros', :action => 'index' } , :caption => 'Carros Tutorial', :after => :activity, :param => :project_id
+  # #############
+  # Para aparecer no menu de aplicação
+  # #############
+  menu :top_menu, :carros, "/carros", :caption => 'Carros Tutorial'
 end
